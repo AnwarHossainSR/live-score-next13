897dd378db3c7341e579a80cede48bebaface141
Restyled.io <commits@restyled.io>
2023-09-13 11:08:38 +0000

    Restyled by clang-format

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'live-score-next-13.vercel.app', // FIXME: Change to the production URL
-  generateRobotsTxt: true,
+  siteUrl : 'live-score-next-13.vercel.app', // FIXME: Change to the production
+                                             // URL
+  generateRobotsTxt : true,
 };
