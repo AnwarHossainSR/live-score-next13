fa707f562265a1f3dcb219f18f1157b475b7a0b1
Restyled.io <commits@restyled.io>
2023-09-13 10:47:33 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
+  images : {
+    remotePatterns : [
       {
-        protocol: 'https',
-        hostname: 'crests.football-data.org',
-        port: '',
-        pathname: '/*',
+        protocol : 'https',
+        hostname : 'crests.football-data.org',
+        port : '',
+        pathname : '/*',
       },
       {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/*/**',
+        protocol : 'https',
+        hostname : 'upload.wikimedia.org',
+        port : '',
+        pathname : '/*/**',
       },
       {
-        protocol: 'https',
-        hostname: 'media.wired.com',
-        port: '',
-        pathname: '/*/**',
+        protocol : 'https',
+        hostname : 'media.wired.com',
+        port : '',
+        pathname : '/*/**',
       },
       {
-        protocol: 'https',
-        hostname: 's.yimg.com',
-        port: '',
-        pathname: '/*/**',
+        protocol : 'https',
+        hostname : 's.yimg.com',
+        port : '',
+        pathname : '/*/**',
       },
       {
-        protocol: 'https',
-        hostname: 'media.npr.org',
-        port: '',
-        pathname: '/*/**',
+        protocol : 'https',
+        hostname : 'media.npr.org',
+        port : '',
+        pathname : '/*/**',
       },
     ],
   },
